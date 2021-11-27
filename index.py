233cc7463125e5caf5919516b5b7331d87c4670e
Dumindu98 <53207557+Dumindu98@users.noreply.github.com>
2021-11-27 13:09:07 +0530

    index

---
+++ index.py
@@ -0,0 +1,118 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def webpage1(request):
+    return render(request, 'index.html')
+
+
+def webpage2(request):
+    return render(request, 'user-login.html')
+
+
+def webpage3(request):
+    return render(request, 'login-wireframe.html')
+
+
+def webpage4(request):
+    return render(request, 'generate-umls.html')
+
+
+def webpage5(request):
+    return render(request, 'wireframe-intro.html')
+
+
+def webpage6(request):
+    return render(request, 'payment-wireframe.html')
+
+
+def webpage7(request):
+    return render(request, 'contact-us-wireframe.html')
+
+
+def webpage8(request):
+    return render(request, 'HCI_Test.html')
+
+
+def webpage9(request):
+    return render(request, 'web-interface.html')
+
+
+def webpage10(request):
+    return render(request, 'wireframes-suggestion.html')
+
+
+def webpage11(request):
+    return render(request, 'HCI-rule-based.html')
+
+
+def webpage12(request):
+    return render(request, 'video-conference.html')
+
+
+def webpage13(request):
+    return render(request, 'UML-API.html')
+
+
+def webpage14(request):
+    return render(request, 'home-wireframe.html')
+
+
+def webpage15(request):
+    return render(request, 'video-display-wireframe.html')
+
+
+def webpage16(request):
+    return render(request, 'about-us-wireframe.html')
+
+
+def webpage17(request):
+    return render(request, 'add-to-cart-wireframe.html')
+
+
+def webpage18(request):
+    return render(request, 'feedback-wireframe.html')
+
+
+def webpage19(request):
+    return render(request, 'paper-prototype.html')
+
+
+def webpage20(request):
+    return render(request, 'HCI-results-representation.html')
+
+
+def webpage21(request):
+    return render(request, 'web-interface-preview.html')
+
+
+def webpage22(request):
+    return render(request, 'video_display.html')
+
+
+def webpage23(request):
+    return render(request, 'Location_Wireframe.html')
+
+
+def webpage24(request):
+    return render(request, 'Settings_Wireframe.html')
+
+
+def webpage25(request):
+    return render(request, 'Blog_Wireframe.html')
+
+
+def webpage26(request):
+    return render(request, 'register.html')
+
+
+def webpage27(request):
+    return render(request, 'web-interface-editor.html')
+
+
+def webpage28(request):
+    return render(request, 'generate-requirement-doc.html')
+
+def webpage29(request):
+    return render(request, 'Undefined-wireframe.html')
+
